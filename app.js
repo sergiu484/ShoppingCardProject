b3465ce816a285b688543183c90af7d0ddea5ee3
Ghimp Sergiu <ghimpsergiu@gmail.com>
2020-04-19 19:16:04 +0300

    LogicCart( Remove-Items + AddItem + RemoveItem) Button(ClearCart + RemoveItems)

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ class UI {
             <div class="img-container">
                 <img src=${product.image} alt="product" class="product-img" />
                 <button class="bag-btn" data-id=${product.id} > 
-                    <i class="fa fa-shopping-cart"></i> add to bag 
+                    <i class="fa fa-shopping-cart"></i> add to cart 
                 </button>
             </div>
             <h3> ${product.title} </h3>
@@ -130,7 +130,65 @@ class UI {
     cartOverlay.classList.remove("transparentBcg");
     cartDOM.classList.remove("showCart");
   }
+  cartLogin() {
+    //clear cart btn
+    clearCartBtn.addEventListener("click", () => this.clearCart());
+    //cart functionallity
+    cartContent.addEventListener("click", (event) => {
+      if (event.target.classList.contains("remove-item")) {
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        cartContent.removeChild(removeItem.parentElement.parentElement);
+        this.removeItem(id);
+      } else if (event.target.classList.contains("fa-chevron-up")) {
+        let addAmount = event.target;
+        let id = addAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount + 1;
+        Storage.saveCart(cart);
+        this.setCartValues(cart);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
+      } else if (event.target.classList.contains("fa-chevron-down")) {
+        let lowerAmount = event.target;
+        let id = lowerAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount - 1;
+        if (tempItem.amount > 0) {
+          Storage.saveCart(cart);
+          this.setCartValues(cart);
+          lowerAmount.previousElementSibling.innerText = tempItem.amount;
+        } else {
+          cartContent.removeChild(lowerAmount.parentElement.parentElement);
+          this.removeItem(id);
+        }
+      }
+    });
+  }
+  clearCart() {
+    let cartItems = cart.map((item) => item.id);
+    cartItems.forEach((id) => this.removeItem(id));
+    console.log(cartContent.children);
+    while (cartContent.children.length > 0) {
+      cartContent.removeChild(cartContent.children[0]);
+    }
+
+    this.hideCart();
+  }
+  removeItem(id) {
+    cart = cart.filter((item) => item.id !== id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleBtn(id);
+    button.disabled = false;
+    button.innerHTML = `
+            <i class="fa fa-shopping-cart"></i>Add To Cart
+    `;
+  }
+  getSingleBtn(id) {
+    return buttonsDOM.find((button) => button.dataset.id === id);
+  }
 }
+
 // local storage
 class Storage {
   static saveProducts(products) {
@@ -163,5 +221,6 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(() => {
       ui.getBagButtons();
+      ui.cartLogin();
     });
 });
